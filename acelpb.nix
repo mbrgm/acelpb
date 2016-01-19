bfbb58c0c5c2cc7f5bda9c6942be4708bf52644b
Augustin Borsu <aborsu@acelpb.com>
2016-01-19 23:10:01 +0100

    fix spelling

--- acelpb.nix
+++ acelpb.nix
@@ -24,6 +24,7 @@ in
     vim
     sbt
     wget
+    docker
   ];
 
   virtualisation.docker.enable = true;
@@ -35,14 +36,14 @@ in
     requires = [ "docker.service" ];
     preStart = ''${pkgs.coreutils}/bin/mkdir -p /var/lib/sonarqube-docker/conf ; \
                  ${pkgs.coreutils}/bin/mkdir -p /var/lib/sonarqube-docker/data ; \
-                 ${pkgs.coreutils}/bin/mkdir -p /var/lib/sonarqube-docker/extentions ; \
+                 ${pkgs.coreutils}/bin/mkdir -p /var/lib/sonarqube-docker/extensions ; \
                  ${pkgs.coreutils}/bin/chown -R 1000 /var/lib/sonarqube-docker ; \
                  ${pkgs.docker}/bin/docker pull sonarqube'';
     serviceConfig = {
       ExecStart = ''${pkgs.docker}/bin/docker run --name sonarqube-docker -p 9000:9000 -p 9092:9092 \
                       -v /var/lib/sonarqube-docker/conf:/opt/sonarqube/conf \
                       -v /var/lib/sonarqube-docker/data:/opt/sonarqube/data \
-                      -v /var/lib/sonarqube-docker/extentions:/opt/sonarqube/extentions \
+                      -v /var/lib/sonarqube-docker/extensions:/opt/sonarqube/extensions \
                       sonarqube
       '';
       ExecStop = ''${pkgs.docker}/bin/docker stop -t 2 sonarqube-docker ; ${pkgs.docker}/bin/docker rm -f sonarqube-docker'';
