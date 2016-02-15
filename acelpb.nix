230220fc9068607695038898db6b9d3a8306271c
Augustin Borsu <aborsu@ulb.ac.be>
2016-02-15 14:12:05 +0100

    add python3

--- acelpb.nix
+++ acelpb.nix
@@ -28,7 +28,7 @@ in
       enable = true;
       port = 2711;
       listenAddress = "localhost";
-      packages = [ pkgs.stdenv pkgs.git pkgs.jdk config.programs.ssh.package pkgs.nix pkgs.sbt pkgs.maven pkgs.vim ];
+      packages = [ pkgs.stdenv pkgs.git pkgs.jdk config.programs.ssh.package pkgs.nix pkgs.sbt pkgs.maven pkgs.vim pkgs.python3 ];
     };
     
     postgresql = {
