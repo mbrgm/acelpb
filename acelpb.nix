c1e64a18261ad5e3772a17002a77050087c7a2d4
Augustin Borsu <a.borsu@gmail.com>
2016-10-01 12:40:51 +0200

    perf(https): improve https security

--- acelpb.nix
+++ acelpb.nix
@@ -95,9 +95,22 @@ in
       httpConfig = ''
         error_log /var/log/nginx/error.log;
 
+        ssl_session_cache shared:ssl_session_cache:10m;
+
+        # Diffie-Hellman parameter for DHE ciphersuites, recommended 2048 bits
+        ssl_dhparam /etc/nginx/ssl/dhparam.pem;
+
+        ssl_ciphers 'ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-DSS-AES128-GCM-SHA256:kEDH+AESGCM:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA:ECDHE-ECDSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-DSS-AES128-SHA256:DHE-RSA-AES256-SHA256:DHE-DSS-AES256-SHA:DHE-RSA-AES256-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:AES128-SHA256:AES256-SHA256:AES128-SHA:AES256-SHA:AES:CAMELLIA:DES-CBC3-SHA:!aNULL:!eNULL:!EXPORT:!DES:!RC4:!MD5:!PSK:!aECDH:!EDH-DSS-DES-CBC3-SHA:!EDH-RSA-DES-CBC3-SHA:!KRB5-DES-CBC3-SHA';
+        ssl_prefer_server_ciphers on;
+
+        add_header Strict-Transport-Security "max-age=63072000; includeSubDomains; preload" always;
+
         server {
-          listen 80;
-          server_name *.acelpb.com;
+          listen 80	default_server;
+          listen [::]:80	default_server;
+          server_name _;
+
+          add_header HTTPS-ONLY "BAD_CLIENT";
 
           location /.well-known/acme-challenge {
             root /var/www/acme.pastespace.org;
@@ -109,29 +122,12 @@ in
         }
 
         server {
-          listen 80	default_server;
-          listen [::]:80	default_server;
-          server_name _;
-
-          location /.well-known/acme-challenge {
-            root /var/www/acme.pastespace.org;
-          }
-
-          location / {
-            return 301 https://www.acelpb.com;
-          }
-        }
-
-
-        server {
           listen 443 ssl;
           listen [::]:443 ssl;
           server_name owncloud.acelpb.com;
 
           ssl_certificate  /var/lib/acme/acelpb.com/fullchain.pem;
           ssl_certificate_key /var/lib/acme/acelpb.com/key.pem;
-
-          add_header Strict-Transport-Security "max-age=63072000; includeSubdomains; preload";
 
           # set max upload size
           client_max_body_size 10G;
