7d383c45d03ad06e741d92fde9be214dea9a3b29
Augustin Borsu <a.borsu@gmail.com>
2016-12-03 13:38:46 +0100

    fix(owncloud-docker): error on restart
    Moved erasing of old image to the start of the script.
    The service was entering a faling loop if it failed to delete
    the image on exiting.

--- owncloud.nix
+++ owncloud.nix
@@ -2,14 +2,14 @@
 {
   systemd.services.owncloud-docker = {
     wantedBy = [ "multi-user.target" ];
-    description = "Containerized sonarqube server";
+    description = "Containerized owncloud server";
     after = [ "docker.service" ];
     requires = [ "docker.service" ];
-    preStart = ''${pkgs.coreutils}/bin/mkdir -p /var/lib/owncloud-docker/apps ; \
+    preStart = ''${pkgs.docker}/bin/docker rm -f owncloud-docker || true;
+                 ${pkgs.coreutils}/bin/mkdir -p /var/lib/owncloud-docker/apps ; \
                  ${pkgs.coreutils}/bin/mkdir -p /var/lib/owncloud-docker/data ; \
                  ${pkgs.coreutils}/bin/mkdir -p /var/lib/owncloud-docker/config ; \
-                 ${pkgs.coreutils}/bin/chown -R 1000 /var/lib/sonarqube-docker ; \
-                 ${pkgs.docker}/bin/docker pull owncloud'';
+                 ${pkgs.coreutils}/bin/chown -R 1000 /var/lib/sonarqube-docker'';
     serviceConfig = {
       ExecStart = ''${pkgs.docker}/bin/docker run --name owncloud-docker -p 2712:80 \
                       -v /var/lib/owncloud-docker/apps:/var/www/html/apps \
@@ -17,7 +17,7 @@
                       -v /var/lib/owncloud-docker/data:/var/www/html/data \
                       owncloud:9.0
       '';
-      ExecStop = ''${pkgs.docker}/bin/docker stop -t 2 owncloud-docker ; ${pkgs.docker}/bin/docker rm -f owncloud-docker'';
+      ExecStop = ''${pkgs.docker}/bin/docker stop -t 2 owncloud-docker'';
     };
   };
 }
