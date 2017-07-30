a670c5bab7db389edcc838d9de5e81b9c3977c61
Augustin Borsu <a.borsu@gmail.com>
2017-07-30 21:21:50 +0200

    WIP

--- acelpb.nix
+++ acelpb.nix
@@ -5,13 +5,12 @@
   imports =
     [
       <nixpkgs/nixos/modules/profiles/headless.nix>
+      ./nextcloud-service.nix
     ];
 
   networking.firewall.allowPing = true;
   networking.firewall.allowedTCPPorts = [ 22 80 443 ];
   
-  services.nginx.enable = true;
-
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
   services.openssh.permitRootLogin = "yes";
@@ -19,4 +18,9 @@
   # Set your time zone.
   time.timeZone = "Europe/Amsterdam";
 
+  services.lighttpd = {
+    enable = true;
+    nextcloud.enable = true;
+    # nextcloud.package = (import ./my-nextcloud.nix);
+  };
 }
